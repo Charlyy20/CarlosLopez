861ef47836851d86db2136ee55f56cbcdabaa6ef
Carlos <carlosl200400@gmail.com>
2024-05-01 22:29:06 -0300

    Register Config and More

--- Project/PreEntrega/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-def home (request): 
-    query = models.Comision.objects.all()
-    context = {"comisiones": query}
-    return render(request, 'PreEntrega/index.html', context)
--- Project/coonfig/urls.py
+++ Project/coonfig/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('PreEntrega/', include('PreEntrega.urls')),
+    path('register/', include('register.urls')),
 ]
